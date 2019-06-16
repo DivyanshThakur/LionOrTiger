6f45535f93b92a536bef29f87ff3ce6dd9d732c6
Divyansh Thakur <divyansh2140@gmail.com>
2019-06-16 20:38:19 +0530

    Added Player selection option. 2 players can now simultaneously play with the LionOrTiger Game.

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -8,17 +8,30 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    enum Player {
+        ONE,TWO;
+    }
+
+    Player currentPlayer = Player.ONE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 }
 
-    public void imgClicked(View imageView){
+    public void imgClicked(View imageView) {
         ImageView tappedImg = (ImageView) imageView;
         tappedImg.setTranslationX(-2000);
-        tappedImg.setImageResource(R.drawable.tiger);
 
-        tappedImg.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+        if (currentPlayer == Player.ONE) {
+            tappedImg.setImageResource(R.drawable.lion);
+            currentPlayer = Player.TWO;
+        } else if (currentPlayer == Player.TWO) {
+            tappedImg.setImageResource(R.drawable.tiger);
+            currentPlayer = Player.ONE;
+        }
+        tappedImg.animate().translationXBy(2000).alpha(1).rotationBy(3600).setDuration(1000);
     }
 }
