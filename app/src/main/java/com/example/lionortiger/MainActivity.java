7c38d5f469187e2df48e1fdc4d8a08dfffb6e21a
Divyansh Thakur <divyansh2140@gmail.com>
2019-06-17 17:48:05 +0530

    Added Tag feature. Added currentPlayer Array. Implimenting the win system.

--- app/src/main/java/com/example/lionortiger/MainActivity.java
+++ app/src/main/java/com/example/lionortiger/MainActivity.java
@@ -10,10 +10,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     enum Player {
-        ONE,TWO;
+        ONE,TWO
     }
 
     Player currentPlayer = Player.ONE;
+
+    Player[] playerChoices = new Player[9];
+
+    int[][] winnerRowsColumns = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +27,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void imgClicked(View imageView) {
         ImageView tappedImg = (ImageView) imageView;
-        tappedImg.setTranslationX(-2000);
+
+        int tiTag = Integer.parseInt(tappedImg.getTag().toString());
+        playerChoices[tiTag] = currentPlayer;
 
         if (currentPlayer == Player.ONE) {
+            tappedImg.setTranslationX(-2000);
             tappedImg.setImageResource(R.drawable.lion);
             currentPlayer = Player.TWO;
+
+            tappedImg.animate().translationXBy(2000).alpha(1).rotationBy(3600).setDuration(1000);
         } else if (currentPlayer == Player.TWO) {
+
+            tappedImg.setTranslationX(2000);
             tappedImg.setImageResource(R.drawable.tiger);
             currentPlayer = Player.ONE;
+
+            tappedImg.animate().translationXBy(-2000).alpha(1).rotationBy(3600).setDuration(1000);
         }
-        tappedImg.animate().translationXBy(2000).alpha(1).rotationBy(3600).setDuration(1000);
+
     }
 }
